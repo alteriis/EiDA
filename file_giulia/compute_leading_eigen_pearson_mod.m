3ed4ed3213fee3cb617e61a5edfa27b680c9dedb
Giulia de Sanctis <giuliadesanctis@Giulias-MacBook-Pro.local>
2023-04-15 11:38:00 +0400

    small corrections

--- file_giulia/compute_leading_eigen_pearson_mod.m
+++ file_giulia/compute_leading_eigen_pearson_mod.m
@@ -7,30 +7,24 @@ n = size(timeseries,1);
 
 leading_eigenvectors = zeros(n_channels,n);
 
-if(verbose)
-    figure
-end
-
-
 for t=1:n
     lower_bound = max(t-half_window_size,1);
     upper_bound = min(t+half_window_size,n);
     istantaneous_conn_matrix= corrcoef(timeseries(lower_bound:upper_bound,:));
     if(verbose)
+        subplot(1,2,1);
         imagesc(istantaneous_conn_matrix);% here you have to do the same plots as the matrix with hilbert transform, moreover, add the timeseries as I asked
-        add_labels (8, group_size, label_names) % here I've filled it with the parameters that suit the dataset us_economy
-        % the above parameters have to be fished from somewhere
+        add_labels (size(group_size,2), group_size, label_names); 
+        pause(0.1)
+        subplot(1,2,2);
+        plot(timeseries);
+        title('Signals Over Time'); 
         l = xline(t, 'LineWidth', 2);
-        pause(0.15)
+        pause(0.1)
         delete(l);
     end
     [v1,~]=eigs(istantaneous_conn_matrix,1); % here you may want to specify more than 1 eigenvector
     leading_eigenvectors(:,t) = v1; 
 end
-
-end
-
-
-
 
 
